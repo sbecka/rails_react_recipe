6a9b88ce53d5074b7b0110d36c9275ae1fac744d
Sabrina Becker <sbecka8@gmail.com>
2020-06-12 20:24:09 -0400

    Configure React and add homepage with initial styles

---
+++ app/javascript/components/App.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Routes from "../routes/Index";
+
+// This component will be rendered at the entry point of the aplication, 
+// thereby making the routes available whenever the application is loaded.
+export default props => <>{Routes}</>;
