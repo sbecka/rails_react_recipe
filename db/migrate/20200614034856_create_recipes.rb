b934f5c85907488c11322b097506bfa5ebdc858e
Sabrina Becker <sbecka8@gmail.com>
2020-06-13 23:59:56 -0400

    Update NOT NULL on columns and migrate recipe db

--- db/migrate/20200614034856_create_recipes.rb
+++ db/migrate/20200614034856_create_recipes.rb
@@ -1,10 +1,10 @@
 class CreateRecipes < ActiveRecord::Migration[6.0]
   def change
     create_table :recipes do |t|
-      t.string :name
-      t.text :ingredients
-      t.text :instruction
-      t.string :image
+      t.string :name, null: false
+      t.text :ingredients, null: false
+      t.text :instruction, null: false
+      t.string :image, default: 'https://www.betterbeanco.com/wp-content/uploads/2015/06/vegan-chile-cheese-fries-Noshtopia500x650.jpg'
 
       t.timestamps
     end
