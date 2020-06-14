9ce31357494946b4db67869cf47c784e6b4ac8e7
Sabrina Becker <sbecka8@gmail.com>
2020-06-13 23:53:06 -0400

    Create Recipe model and add validation to data

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Routes from "../routes/Index";
 
-// This component will be rendered at the entry point of the aplication, 
-// thereby making the routes available whenever the application is loaded.
+// This component will be rendered at the entry point of the app, 
+// thereby making the routes available whenever the app is loaded.
 export default props => <>{Routes}</>;
